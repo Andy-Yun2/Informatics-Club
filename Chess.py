b1c7cb354d7fa80b3b318b91796236c97ff877d4
196586 <196586@cloud.rngitoto.school.nz>
2026-02-16 14:15:38 +1300

    chess

---
+++ Chess.py
@@ -0,0 +1,13 @@
+
+num_ans = int(input())
+coord_list = []
+output_list = []
+
+for i in range(num_ans):
+    coord = input()
+    coord_list.append(coord)
+
+for i in range(num_ans):
+    split_coord = coord_list[i].split()
+    if split_coord[0] / 2 :
+
